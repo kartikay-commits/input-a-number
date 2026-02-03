3a44d941b43b08eb077069d4648f2ad0789539de
kartikay singh <kartikaysin566@gmail.com>
2026-02-03 19:53:51 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main{
+
+	public static void main(String[] args) {
+	    
+		Scanner sc= new Scanner(System.in);
+		
+	System.out.println("Enter an integer :");
+	
+	int number=sc.nextInt();
+	
+	System.out.println("You entered :" + number);
+	
+	sc.close();
+	}
+}
